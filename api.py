f81d85db6d107cd3345f1253f982a9e6c132bc83
Hifi58 <anaisbiquena@macbook-pro-anais.home>
2022-03-16 14:15:42 +0100

    basic get id

--- api.py
+++ api.py
@@ -2,10 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-#get
+###################
+# Welcome message #
+###################
+
 @app.get("/")
 async def hello_world():
     return {"Welcome on HELP Your Meal API !"}
+
+###################
+## Get for Stock ##
+###################
+
+@app.get("/stock/{stock_id}")
+async def get_stock(stock_id: int):
+    return {"stock_id": stock_id}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
